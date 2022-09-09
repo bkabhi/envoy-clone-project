28370df8a8cd5a97327a7e3a5ce298d14cd598de
bkabhi <bkabhijeet9801@gmail.com>
2022-09-09 14:28:49 +0530

    update

---
+++ nav/components.js
@@ -0,0 +1,332 @@
+function navbar(){
+    return `
+    <div id="home-navbar-div1">
+    <a href="../home/home.html"><img src="https://envoy.com/images/logo-new.svg" alt="" id="home-navbar-icon"></a>
+</div>
+<div id="home-navbar-div2">
+    <div id="navbar-hiring">
+        <a href="../jobs/jobs.html" class="navbar-anchor">We're hiring'! </a>
+    </div>
+    <div id="navbar-products" class="drop-hower-div">
+            <a href="../store/store.html" class="navbar-anchor">Products</a>
+            
+            <div class="drop-down-div-content-parent">
+                <div class="drop-down-div-content">
+                    <a href="">
+                        <img height="50px" width="50px" src="https://envoy.com/images/icons/products/visitors-app.png" alt="">
+                        <div class="drop-content-text">
+                            <div>Visitors</div>
+                            <div>Visitor management solutions for any front desk</div>
+                        </div>
+                    </a>
+                    <a href="">
+                        <img height="50px" width="50px" src="https://envoy.com/images/icons/products/protect-app.png" alt="">
+                        <div class="drop-content-text">
+                            <div>Protect</div>
+                            <div>Manage hybrid schedules and keep employees safe</div>
+                        </div>
+                    </a>
+                    <a href="">
+                        <img height="50px" width="50px" src="https://envoy.com/images/icons/products/desks-app.png" alt="">
+                        <div class="drop-content-text">
+                            <div>Desks</div>
+                            <div>Easy desk booking for a flexible workplace</div>
+                        </div>
+                    </a>
+                    <a href="">
+                        <img height="50px" width="50px" src="https://envoy.com/images/icons/products/rooms-app.png" alt="">
+                        <div class="drop-content-text">
+                            <div>Rooms</div>
+                            <div>Room booking make easier for your team</div>
+                        </div>
+                    </a>
+                    <a href="">
+                        <img height="50px" width="50px" src="https://envoy.com/images/icons/products/deliveries-app.png" alt="">
+                        <div class="drop-content-text">
+                            <div>Deliveries</div>
+                            <div>A new way to organize incomig office deliveries</div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+    </div>
+    <div id="navbar-solutions" class="drop-hower-div">
+        <a href="#" class="navbar-anchor">Solutions</a>
+
+        <div class="drop-down-div-content-parent">
+                <div class="drop-down-div-content">
+                    <a href="">
+                        <img width="20px"src="https://envoy.com/images/icons/24px/red/globe.svg" alt="">
+                        <div >
+                            <div class="drop--content-text">Hybrid Work</div>
+                            <div></div>
+                        </div>
+                    </a>
+                    <a href="">
+                        <img width="20px"src="https://envoy.com/images/icons/24px/red/office.svg" alt="">
+                        <div >
+                            <div class="drop--content-text">Support enterprise needs</div>
+                            <div></div>
+                        </div>
+                    </a>
+                    <a href="">
+                        <img width="20px" src="https://envoy.com/images/icons/24px/red/shield.svg" alt="">
+                        <div >
+                            <div class="drop--content-text">Enhance security</div>
+                            <div></div>
+                        </div>
+                    </a>
+                    <a href="">
+                        <img width="20px" src="https://envoy.com/images/icons/24px/red/rooms.svg" alt="">
+                        <div >
+                            <div class="drop--content-text">Space management</div>
+                            <div></div>
+                        </div>
+                    </a>
+                    <a href="">
+                        <img width="20px" src="https://envoy.com/images/icons/24px/red/pulse.svg" alt="">
+                        <div >
+                            <div class="drop--content-text">Workplace health and safety</div>
+                            <div></div>
+                        </div>
+                    </a>
+                    <a href="">
+                        <img width="20px" src="https://envoy.com/images/icons/16px/red/sparkles.svg" alt="">
+                        <div >
+                            <div class="drop--content-text">Workplace experience</div>
+                            <div></div>
+                        </div>
+                    </a>
+                    <a href="">
+                        <img width="20px" src="https://envoy.com/images/icons/24px/red/document.svg" alt="">
+                        <div >
+                            <div class="drop--content-text">Meet compliance requirments</div>
+                            <div></div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+    </div>
+    <div id="navbar-resources" class="drop-hower-div">
+        <a href="#" class="navbar-anchor">Resources</a>
+        
+        <div class="drop-down-div-content-parent">
+                <div class="drop-down-div-content">
+                    <a href="">
+                        <img  width="20px"src="https://envoy.com/images/icons/24px/red/office.svg" alt="">
+                        <div >
+                            <div class="drop--content-text">Workplace trends report</div>
+                            <div></div>
+                        </div>
+                    </a>
+                    <a href="">
+                        <img  width="20px" src="https://envoy.com/images/icons/24px/red/pen-nib.svg" alt="">
+                        <div >
+                            <div class="drop--content-text">Blog</div>
+                            <div></div>
+                        </div>
+                    </a>
+                    <a href="">
+                        <img  width="20px" src="https://envoy.com/images/icons/24px/red/book.svg" alt="">
+                        <div >
+                            <div class="drop--content-text">Ebooks</div>
+                            <div></div>
+                        </div>
+                    </a>
+                    <a href="">
+                        <img  width="20px" src="https://envoy.com/images/icons/24px/red/webinar.svg" alt="">
+                        <div >
+                            <div class="drop--content-text">Webinars</div>
+                            <div></div>
+                        </div>
+                    </a>
+                    <a href="">
+                        <img  width="20px" src="https://envoy.com/images/icons/24px/red/announcements.svg" alt="">
+                        <div >
+                            <div class="drop--content-text">Case studies</div>
+                            <div></div>
+                        </div>
+                    </a>
+                    <a href="">
+                        <img  width="20px" src="https://envoy.com/images/icons/16px/red/sparkles.svg" alt="">
+                        <div >
+                            <div class="drop--content-text">Data insights</div>
+                            <div></div>
+                        </div>
+                    </a>
+                    <a href="">
+                        <img  width="20px" src="https://envoy.com/images/icons/24px/red/data.svg" alt="">
+                        <div >
+                            <div class="drop--content-text">Return to Workplace Index</div>
+                            <div></div>
+                        </div>
+                    </a>
+                    <a href="">
+                        <img  width="20px" src="https://envoy.com/images/icons/24px/red/megaphone.svg" alt="">
+                        <div >
+                            <div class="drop--content-text">Product News</div>
+                            <div></div>
+                        </div>
+                    </a>
+                    <a href="">
+                        <img  width="20px" src="https://envoy.com/images/icons/24px/red/globe.svg" alt="">
+                        <div >
+                            <div class="drop--content-text">Hybrid Work Resource Hub</div>
+                            <div></div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+    </div>
+    <div id="navbar-demo" class="drop-hower-div">
+        <a href="#" class="navbar-anchor">Demo</a>
+    </div>
+    <div id="navbar-pricing" class="drop-hower-div">
+        <a href="#" class="navbar-anchor">Pricing</a>
+        
+        <div class="drop-down-div-content-parent">
+                <div class="drop-down-div-content">
+                    <a href="">
+                        <img  width="20px" src="https://envoy.com/images/icons/24px/red/tag.svg" alt="">
+                        <div >
+                            <div class="drop--content-text">Plans and pricing</div>
+                            <div></div>
+                        </div>
+                    </a>
+                    <a href="">
+                        <img width="20px" src="https://envoy.com/images/icons/24px/red/calculator.svg" alt="">
+                        <div >
+                            <div class="drop--content-text">Get a quote</div>
+                            <div></div>
+                        </div>
+                    </a>
+                    <a href="">
+                        <img width="20px" src="https://envoy.com/images/icons/24px/red/phone.svg" alt="">
+                        <div >
+                            <div class="drop--content-text">Call sales: (877)652-2808</div>
+                            <div></div>
+                        </div>
+                    </a>
+                    <a href="">
+                        <img width="20px" src="https://envoy.com/images/icons/24px/red/envelope.svg" alt="">
+                        <div >
+                            <div class="drop--content-text">Contact us</div>
+                            <div></div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+    </div>
+    <div id="navbar-login" class="drop-hower-div">
+        <a href="#" class="navbar-anchor">Log In</a>
+    </div>
+    <div id="navbar-get-started" class="drop-hower-div">
+        <a href="#" class="navbar-anchor">Get started</a>
+    </div>
+</div>`
+}
+
+
+
+const footer = `
+<div id="heading">
+        <div class="footer-form-div-upper">
+            <div>
+                <h2 class="footer-form-div-h2">Try Envoy for free</h2>
+                <p class="footer-form-div-p">Transforming your workplace is easier than you think. Get started in
+                    minutes.</p>
+                <br>
+            </div>
+            <div>
+                <input class="footer-form-div-inp" type="text" placeholder="Enter your Email" />
+                <button class="footer-form-div-btn">Get started</button>
+            </div>
+        </div>
+        <hr>
+        <div id="contact"><i class="fa-solid fa-phone"></i>&nbsp; &nbsp;<p>Contact our sales team today at <span>(877)
+                    652-2808</span></p>
+        </div>
+        <hr>
+        <footer class="footer-container">
+            <div class="card1">
+                <ul>
+                    <h3>Products</h3>
+                    <li>Desks</li>
+                    <li>Visitors</li>
+                    <li>Rooms</li>
+                    <li>Deliveries</li>
+                    <li>Mobile</li>
+                    <li>Integrations</li>
+                    <li>Partnerships</li>
+                    <li>Pricing</li>
+                    <li>Get a quote</li>
+                </ul>
+            </div>
+            <div class="card2">
+
+                <ul>
+                    <h3>Solutions</h3>
+                    <li>Hybrid work</li>
+                    <li>Enterprise</li>
+                    <li>Security</li>
+                    <li>Space management</li>
+                    <li>Health and safety</li>
+                    <li>Workplace experience</li>
+                    <li>Compliance</li>
+                    <li>Touchless sign-in</li>
+                    <li>Access control</li>
+                    <li>Efficiency</li>
+                    <li>Experience</li>
+                </ul>
+            </div>
+            <div class="card3">
+
+                <ul>
+                    <h3>Resources</h3>
+                    <li>Hybrid work resource hub</li>
+                    <li>Help center</li>
+                    <li>Return to workplace Index</li>
+                    <li>Envoy Store</li>
+                    <li>Customer stories</li>
+                    <li>Demo</li>
+                    <li>Compliance tips</li>
+                    <li>Developers</li>
+                    <li>Glossary</li>
+                </ul>
+            </div>
+            <div class="card4">
+
+                <ul>
+                    <h3>Company</h3>
+                    <li>About Envoy</li>
+                    <li>Vision</li>
+                    <li>Jobs</li>
+                    <li>Blog</li>
+                    <li>Data security</li>
+                    <li>Press</li>
+                    <li>Partner</li>
+                </ul>
+            </div>
+        </footer>
+    </div>
+    <div id="shopify-section">
+        <!--LEFT SECTION-->
+        <div class="anchor">
+            <a href="https://envoy.com/contact/">Contact</a>
+            <a href="https://status.envoy.com/">Status</a>
+            <a href="https://envoy.com/tos/">Terms of Service</a>
+            <a href="https://envoy.com/privacypolicy/">Privacy Policy</a>
+        </div>
+
+        <!--RIGHT SECTION-->
+        <div class="icons">
+            <a href="https://www.facebook.com/SignWithEnvoy/"><i class="fa-brands fa-facebook"></i></a>
+            <a href="https://twitter.com/envoy/"><i class="fa-brands fa-twitter"></i></i></a>
+            <a href="https://www.linkedin.com/company/envoy-inc/"><i class="fa-brands fa-linkedin"></i></a>
+        </div>
+    </div>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">`
+
+
+
+export {navbar, footer} 
